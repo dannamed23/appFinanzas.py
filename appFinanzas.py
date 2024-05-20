9f6a28412756a8865f0cbf890119a0b27706ecdf
dannamed23 <dannamed1221@gmail.com>
2024-05-19 19:10:11 -0600

    Update appFinanzas.py

--- appFinanzas.py
+++ appFinanzas.py
@@ -7,6 +7,22 @@ import streamlit as st
 #write: método
 #(): variables
 
+
+
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode("utf-8")
+
+csv = convert_df(my_large_df)
+
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name="large_df.csv",
+    mime="text/csv",
+)
+
 st.title("Ejercicios de Finanzas :symbols:") 
 
 st.header('Materia Ingeniería Financiera')
