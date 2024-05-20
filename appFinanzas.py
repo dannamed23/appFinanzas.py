70b642e0bc6d4b768ce2409294837ebdb020daf1
dannamed23 <dannamed1221@gmail.com>
2024-05-19 19:23:02 -0600

    Update appFinanzas.py

--- appFinanzas.py
+++ appFinanzas.py
@@ -58,6 +58,14 @@ fig.update_layout(
 # Mostrar la figura en Streamlit
 st.plotly_chart(fig)
 
+options = st.multiselect(
+    "What are your favorite colors",
+    ["Green", "Yellow", "Red", "Blue"],
+    ["Yellow", "Red"])
+
+st.write("You selected:", options)
+
+
 
 
 st.title("PISA Analisis :smile:") 
